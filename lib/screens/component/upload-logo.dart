751fd4bb199ce2e8cc0e03b32f2dea660124502c
mvinet <vinetmanon85@gmail.com>
2025-01-22 13:41:19 +0100

    Ajout box cliquable

--- lib/screens/component/upload-logo.dart
+++ lib/screens/component/upload-logo.dart
@@ -6,35 +6,36 @@ class UploadLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+        child: InkWell(
+      onTap: () {},
       child: Container(
-          padding: const EdgeInsets.all(15),
-          width: MediaQuery.of(context).size.width * 0.10,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.blue.shade300),
-            borderRadius: BorderRadius.circular(8),
-            color: Colors.lightBlue.shade50,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.upload_file,
-                    size: 35,
-                    color: Colors.blue.shade700,
-                  ),
-                  SizedBox(height: 10),
-                  const Text(
-                    'Ajouter un Logo',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                ],
+        padding: const EdgeInsets.all(15),
+        width: MediaQuery.of(context).size.width * 0.10,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.blue.shade300),
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.lightBlue.shade50,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.upload_file,
+              size: 35,
+              color: Colors.blue.shade700,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Ajouter un Logo',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
               ),
-            ],
-          )),
-    );
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
