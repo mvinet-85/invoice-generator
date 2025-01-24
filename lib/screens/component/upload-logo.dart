f7677a9a3ffba0071baaa5a8019b9742c54aa813
mvinet <vinetmanon85@gmail.com>
2025-01-24 10:44:46 +0100

    Upload d'image

--- lib/screens/component/upload-logo.dart
+++ lib/screens/component/upload-logo.dart
@@ -1,41 +1,87 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class UploadLogo extends StatelessWidget {
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UploadLogo extends StatefulWidget {
   const UploadLogo({super.key});
+
+  @override
+  State<UploadLogo> createState() => _UploadLogoState();
+}
+
+class _UploadLogoState extends State<UploadLogo> {
+  Uint8List? _imageBytes;
+  File? _imageFile;
+
+  Future<void> _pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedFile =
+        await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      if (kIsWeb) {
+        final bytes = await pickedFile.readAsBytes();
+        setState(() {
+          _imageBytes = bytes;
+        });
+      } else {
+        setState(() {
+          _imageFile = File(pickedFile.path);
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: InkWell(
-      onTap: () {},
-      child: Container(
-        padding: const EdgeInsets.all(15),
-        width: MediaQuery.of(context).size.width * 0.10,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.blue.shade300),
-          borderRadius: BorderRadius.circular(8),
-          color: Colors.lightBlue.shade50,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.upload_file,
-              size: 35,
-              color: Colors.blue.shade700,
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Ajouter un Logo',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
+      child: InkWell(
+        onTap: _pickImage,
+        child: _imageBytes != null || _imageFile != null
+            ? Container(
+                padding: const EdgeInsets.all(15),
+                width: MediaQuery.of(context).size.width * 0.10,
+                child: _imageBytes != null
+                    ? Image.memory(
+                        _imageBytes!,
+                        fit: BoxFit.cover,
+                      )
+                    : Image.file(
+                        _imageFile!,
+                        fit: BoxFit.cover,
+                      ),
+              )
+            : Container(
+                padding: const EdgeInsets.all(15),
+                width: MediaQuery.of(context).size.width * 0.10,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue.shade300),
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.lightBlue.shade50,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.upload_file,
+                      size: 35,
+                      color: Colors.blue.shade700,
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Ajouter un Logo',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
       ),
-    ));
+    );
   }
 }
